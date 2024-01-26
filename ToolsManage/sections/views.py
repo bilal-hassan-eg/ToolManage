8d00f5c910b6e0efbf83d3c203067403319e29b6
Bilal Hassan <rhsan1565@gmail.com>
2024-01-26 19:47:01 +0200

    update

--- ToolsManage/sections/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
-
-def CompanyStatistics(response):
-    data = {'name':'sawa'}
-    return render(response,"sections/statistics.html",data)
